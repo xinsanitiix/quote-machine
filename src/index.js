0baaa67c0ce868844cbac410a4fef0004086520c
xinsanitiix <xinsanitiix@gmail.com>
2025-12-04 00:59:50 +0100

    add basic js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-alert("Have a great day");
+function generateQuote(event) {
+  event.preventDefault();
+
+  new Typewriter("#quote", {
+    strings:
+      "The best time to have planted a tree was 10 years ago, the second best time is today!",
+    autoStart: true,
+    cursor: "",
+    delay: 10,
+  });
+}
+
+let quoteFormElement = document.querySelector("#quote-generator");
+quoteFormElement.addEventListener("submit", generateQuote);
